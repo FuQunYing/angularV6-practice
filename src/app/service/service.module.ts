6c55266d2db2a1ec08c91c261f1bc6f73a1907e5
FuQunYing <fqylovae@outlook.com>
2018-05-21 15:27:24 +0800

    service mosule update

--- src/app/service/service.module.ts
+++ src/app/service/service.module.ts
@@ -1,10 +1,42 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import {AuthGuardService} from './auth-guard.service';
+import {ConsumerManagementService} from './consumer-management.service';
+import {CookieService} from './cookie.service';
+import {DeviceService} from './device.service';
+import {LoginService} from './login.service';
+import {MaintainService} from './maintain.service';
+import {OrderService} from './order.service';
+import {PowerService} from './power.service';
+import {SpeedService} from './speed.service';
 
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
-})
-export class ServiceModule { }
+export {
+  AuthGuardService,
+  ConsumerManagementService,
+  CookieService,
+  DeviceService,
+  LoginService,
+  MaintainService,
+  OrderService,
+  PowerService,
+  SpeedService
+};
+@NgModule()
+export class ServiceModule {
+  static forRoot(): ModuleWithProviders {
+  return {
+    ngModule: ServiceModule,
+    providers: [
+      AuthGuardService,
+      ConsumerManagementService,
+      CookieService,
+      DeviceService,
+      LoginService,
+      MaintainService,
+      OrderService,
+      PowerService,
+      SpeedService
+    ]
+  };
+  }
+}
+
