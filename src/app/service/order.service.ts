862a223b553b2cf62f0e3e5a6bfd5073505eef07
FuQunYing <fqylovae@outlook.com>
2018-05-30 18:43:46 +0800

    order service update

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {CookieService} from './cookie.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class OrderService {
 
-  constructor() { }
+  constructor( private http: HttpClient, private _cookie: CookieService) { }
+  // headers
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'x-access-token': this._cookie.getStorage('token'),
+    'secret': this._cookie.getStorage('secret')
+  });
+
+  /**
+   * 单号查询
+   * @param body
+   * @returns {Promise<any>}
+   */
+  public getOrder(body): Promise<any> {
+    const uri = 'api/v1/order/query';
+    return this.http.post(uri, body, {headers: this.headers})
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  /**
+   * 订单投诉
+   * @param body
+   * @returns {Promise<any>}
+   */
+  public getComplain(body): Promise<any> {
+    const uri = '/api/v1/order/complain';
+    return this.http.post(uri, body, {headers: this.headers})
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.log('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
 }
