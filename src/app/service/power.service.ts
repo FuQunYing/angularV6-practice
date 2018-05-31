a73069a9549845ae0a0d19e6c991a2e17c2dc9bb
FuQunYing <fqylovae@outlook.com>
2018-05-31 17:51:11 +0800

    power service update

--- src/app/service/power.service.ts
+++ src/app/service/power.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import {CookieService} from './cookie.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class PowerService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private _cookie: CookieService) { }
+  // headers
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'x-access-token': this._cookie.getStorage('token'),
+    'secret': this._cookie.getStorage('secret')
+  });
+
+  /**
+   * 获取所有用户
+   * @returns {Promise<Object>}
+   */
+  public getUser(): Promise<any> {
+    const uri = '/api/v1/user?pageSize=99&pageNum=1&step=start';
+    return this.http.get(uri, {headers: this.headers})
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  /**
+   * 获取所有角色
+   * @returns {Promise<any>}
+   */
+  public getRole(): Promise<any> {
+    const uri = '/api/v1/role?pageSize=99&pageNum=1&step=start';
+    return this.http.get(uri, {headers: this.headers})
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  /**
+   * 获取资源
+   * @returns {Promise<any>}
+   */
+  public getRes(): Promise<any> {
+    const uri = '/api/v1/res?pageSize=99&pageNum=1&step=start';
+    return this.http.get(uri, {headers: this.headers})
+      .toPromise()
+      .catch(this.handleError);
+  }
+  handleError(error: any) {
+    console.log('An error occurred:', error);
+    return Promise.reject(error.message || error);
+  }
 }
