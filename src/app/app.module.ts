4bfe4eee9286932882c6165c49aa1f7c326e014b
FuQunYing <fqylovae@outlook.com>
2018-06-01 18:46:31 +0800

    app module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {SharedModule} from './shared/shared.module';
+import {LayoutModule} from './layout/layout.module';
+import {CoreModule} from './core/core.module';
 
 
 @NgModule({
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule.forRoot(),
+    LayoutModule,
+    RouterModule,
+    CoreModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
