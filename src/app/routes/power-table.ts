0bf5c0cf72c92d61a91a3f2a71d4b739f5f6330a
FuQunYing <fqylovae@outlook.com>
2018-06-06 16:58:14 +0800

    power list update

---
+++ src/app/routes/power-table.ts
@@ -0,0 +1,46 @@
+export const POWER_LIST = [
+  {
+    'path': 'device/query',
+    'zh': '设备查询'
+  },
+  {
+    'path': 'device/bind',
+    'zh': '设备绑定'
+  },
+  {
+    'path': 'device/activate',
+    'zh': '生产激活'
+  },
+  {
+    'path': 'device/speed',
+    'zh': '上分速度'
+  },
+  {
+    'path': 'order/query',
+    'zh': '订单查询'
+  },
+  {
+    'path': 'order/complain',
+    'zh': '投诉查询'
+  },
+  {
+    'path': 'cardno/query',
+    'zh': '卡号查询'
+  },
+  {
+    'path': 'cardno/recharge',
+    'zh': '虚拟充值'
+  },
+  {
+    'path': 'maintain/notice',
+    'zh': '运维公告'
+  },
+  {
+    'path': 'maintain/checkgoods',
+    'zh': '第三方商品审核'
+  },
+  {
+    'path': 'maintain/logs',
+    'zh': '支付页面升级日志'
+  }
+];
