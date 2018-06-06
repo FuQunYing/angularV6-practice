cc73f8b73e7d070c564cc27a494e71148f7a9212
FuQunYing <fqylovae@outlook.com>
2018-06-06 16:48:52 +0800

    routes update

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
 import {PagesModule} from './pages/pages.module';
+import {RoutesRoutingModule} from './routes-routing.module';
 
 @NgModule({
   imports: [
+    SharedModule,
+    RoutesRoutingModule,
+    PagesModule
   ],
   declarations: []
 })
