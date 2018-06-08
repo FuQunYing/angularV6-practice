737237e188d54cff1c484acc93de4a19c5c6289e
FuQunYing <fqylovae@outlook.com>
2018-06-08 18:43:39 +0800

    maintain module updatee

--- src/app/routes/maintain/maintain.module.ts
+++ src/app/routes/maintain/maintain.module.ts
@@ -1,15 +1,37 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {SharedModule} from '../../shared/shared.module';
+import {Routes, RouterModule, Router} from '@angular/router';
 import { CheckgoodsComponent } from './checkgoods/checkgoods.component';
 import { EditGoodsComponent } from './edit-goods/edit-goods.component';
 import { LogsComponent } from './logs/logs.component';
 import { NoticeComponent } from './notice/notice.component';
 import { PublicComponent } from './public/public.component';
+import {AuthGuardService} from '../../service/auth-guard.service';
 
+const routes: Routes = [
+  {path: '', redirectTo: 'logs', pathMatch: 'full', canActivate: [AuthGuardService]},
+  {path: 'notice', component: NoticeComponent, canActivate: [AuthGuardService]},
+  {path: 'checkgoods', component: LogsComponent, canActivate: [AuthGuardService]},
+  {path: 'logs', component: LogsComponent, canActivate: [AuthGuardService]},
+  {path: 'public', component: PublicComponent, canActivate: [AuthGuardService]}
+];
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [CheckgoodsComponent, EditGoodsComponent, LogsComponent, NoticeComponent, PublicComponent]
+  declarations: [
+    CheckgoodsComponent,
+    EditGoodsComponent,
+    LogsComponent,
+    NoticeComponent,
+    PublicComponent
+  ],
+  exports: [
+    RouterModule
+  ],
+  entryComponents: [
+    EditGoodsComponent
+  ]
 })
 export class MaintainModule { }
