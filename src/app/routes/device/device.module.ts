bdfaaa3cdaf396e788dfe8ca64a3750578b0e806
FuQunYing <fqylovae@outlook.com>
2018-06-08 18:38:44 +0800

    device module update

--- src/app/routes/device/device.module.ts
+++ src/app/routes/device/device.module.ts
@@ -1,15 +1,44 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {SharedModule} from '../../shared/shared.module';
+import {Routes, RouterModule} from '@angular/router';
 import { ActivateComponent } from './activate/activate.component';
 import { ActivateProductComponent } from './activate-product/activate-product.component';
 import { BindComponent } from './bind/bind.component';
 import { QueryComponent } from './query/query.component';
 import { SpeedComponent } from './speed/speed.component';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {DefaultInterceptor} from '../../core/net/default.interceptor';
+import {AuthGuardService} from '../../service/auth-guard.service';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'query', pathMatch: 'full', canActivate: [AuthGuardService]},
+  {path: 'query', component: QueryComponent, canActivate: [AuthGuardService]},
+  {path: 'activate', component: ActivateComponent, canActivate: [AuthGuardService]},
+  {path: 'speed', component: SpeedComponent, canActivate: [AuthGuardService]},
+  {path: 'activate_product', component: ActivateProductComponent, canActivate: [AuthGuardService]}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule,
+    RouterModule.forChild(routes)
   ],
-  declarations: [ActivateComponent, ActivateProductComponent, BindComponent, QueryComponent, SpeedComponent]
+  declarations: [
+    ActivateComponent,
+    ActivateProductComponent,
+    BindComponent,
+    QueryComponent,
+    SpeedComponent
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers:[
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: DefaultInterceptor,
+      multi: true
+    }
+  ]
 })
 export class DeviceModule { }
