861414a81af9c60ce4eea98d20a2fd3a9b8d973d
FuQunYing <fqylovae@outlook.com>
2018-08-15 19:42:09 +0800

    运维公告（2个）更新

--- src/app/routes/maintain/logs/logs.component.ts
+++ src/app/routes/maintain/logs/logs.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-logs',
-  templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.scss']
+  template: `
+    <nz-card [nzBordered]="false" [nzNoHovering]="true">
+      <ng-template #body>
+        <nz-tabset>
+          <nz-tab *ngFor="let tab of tabs">
+            <ng-template #nzTabHeading>
+              {{tab.name}}
+            </ng-template>
+            <markdown path="{{tab.content}}"></markdown>
+          </nz-tab>
+        </nz-tabset>
+      </ng-template>
+    </nz-card>
+  `
 })
-export class LogsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class LogsComponent {
+  tabs = [
+    {
+      name   : 'MDB',
+      content: '/assets/logs/logs.md'
+    },
+    {
+      name   : '脉冲',
+      content: '/assets/logs/logs.md'
+    },
+    {
+      name   : '云货道',
+      content: '/assets/logs/logs.md'
+    },
+    {
+      name   : '云钩哒',
+      content: '/assets/logs/logs.md'
+    }
+  ];
 }
